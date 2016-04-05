e34f9a6a33452169ebb3f30795ad5819dae9ee7b
sgtmt <ainzuen@gmail.com>
2016-04-06 03:08:15 +0900

    commit

--- count/ViewController.swift
+++ count/ViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var data: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+         data.text=String(atai);
+            }
+    var atai=0;
+    
+    
+    @IBAction func countBtn(plusBtn: AnyObject) {
+        atai += 1;
+        ColorBtn()
+
+        data.text=String(atai);
+        
     }
+    func sadPoint(){
+//        atai-1;
+//        if atai <= 0{
+//        
+//        }
+       
+        
+        }
+    func ColorBtn(){
+        if atai%10 == 0{
+            data.textColor = UIColor.redColor()
+            }else{
+            data.textColor = UIColor.blackColor()
+
+        }
+    }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
