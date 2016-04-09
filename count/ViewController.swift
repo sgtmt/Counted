f430af3903bbfffd984d54e64bc96b49cb7e63d2
sgtmt <ainnuen@gmail.com>
2016-04-10 05:14:09 +0900

    2016/04/10 tapLongGastureとsingleTapが合体してごちゃっている

--- count/ViewController.swift
+++ count/ViewController.swift
@@ -14,8 +14,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        viewSet.backgroundColor=UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         
-         data.text=String(atai);
+        data.text=String(atai);
+            redLabel.text="red :0"
+            blueLabel.text="blue :0"
+            greenLabel.text="green :0"
+            sadBtn.enabled = false
      
             }
     
@@ -24,19 +29,54 @@ class ViewController: UIViewController {
     var blueColor:CGFloat = 0
     var greenColor:CGFloat = 0
     var alphaColor:CGFloat = 0.5
-    
-  
-   
-   
-    
-       @IBOutlet weak var rgbSet: UILabel!
+    @IBOutlet weak var viewSet: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var silderRed: UISlider!
+    @IBOutlet weak var silderGreen: UISlider!
+    @IBOutlet weak var silderBlue: UISlider!
+    @IBOutlet weak var rgbSet: UILabel!
 
-    @IBAction func redBtn(sender: AnyObject) {
+    @IBOutlet weak var sadBtn: UIButton!
+    @IBOutlet weak var rgbView: UIView!
+    @IBAction func redSlider(sender: AnyObject) {
+        viewBackgroundSet()
+        redLabel.text="red :\(silderRed.value)"
+     
+    }
+    func viewBackgroundSet(){
+        viewSet.backgroundColor=UIColor(red: CGFloat(silderRed.value), green: CGFloat(silderGreen.value), blue: CGFloat(silderBlue.value), alpha: 1.0)
+    }
+    @IBAction func blueSlider(sender: AnyObject) {
+        blueLabel.text="blue :\(silderBlue.value)"
+        viewBackgroundSet()
+    }
+    @IBAction func blueLongBtn(sender: AnyObject) {
+        blueColor+=0.1
+        backgroundColor()
+        colorScaning()
+    }
+    @IBAction func greenSlider(sender: AnyObject) {
+        greenLabel.text="green :\(silderGreen.value)"
+        viewBackgroundSet()
+    }
+      @IBAction func redBtn(sender: AnyObject) {
         redColor+=0.1
          backgroundColor()
         colorScaning()
-       
-        
+       }
+    
+    @IBAction func redLongBtn(sender: AnyObject) {
+        redColor+=0.1
+        backgroundColor()
+        colorScaning()
+    }
+    
+    @IBAction func colorReset(sender: AnyObject) {
+       colorDefault()
+        backgroundColor()
+        colorScaning()
     }
     @IBAction func greenBtn(sender: AnyObject) {
         greenColor+=0.1
@@ -44,42 +84,85 @@ class ViewController: UIViewController {
         colorScaning()
 
     }
-    @IBAction func blueBtn(sender: AnyObject) {
+    @IBAction func greenLongBtn(sender: AnyObject) {
+        greenColor+=0.1
+        backgroundColor()
+        colorScaning()
+
+        
+    }
+       @IBAction func blueBtn(sender: AnyObject) {
           blueColor+=0.1
          backgroundColor()
         colorScaning()
     }
     @IBAction func countBtn(plusBtn: AnyObject) {
-        atai += 1;
+        sadParam()
+        addSadBtn()
         ColorBtn()
-        
-        data.text=String(atai);
+        stringData()
             }
+    @IBAction func sadBtn(sender: AnyObject) {
+        sadParam()
+        sadPoint()
+        ColorBtn()
+        stringData()
+    }
     func colorScaning(){
          rgbSet.text="red: \(redColor) green: \(greenColor) blue: \(blueColor)"
     }
+
     func backgroundColor(){
-                self.view.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: alphaColor)
+//                self.view.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: alphaColor)
+           rgbView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: alphaColor)
+    }
+    func colorDefault(){
+        //default
+        redColor = 0
+        blueColor = 0
+        greenColor = 0
     }
     
     func sadPoint(){
-//        atai-1;
-//        if atai <= 0{
-//        
-//        }
-       
+        //--
+        if atai > 0 {
+          atai -= 1;
+        }else{}
         
-        }
+    
+       }
+    func stringData(){
+        //update
+          data.text=String(atai);
+    }
+    func addSadBtn(){
+        //++
+        if atai>=0{
+            atai+=1
+        }else{}
+    }
+
     func ColorBtn(){
-
-
+        //colorSet
+        if atai == 0{
+            
+        }else {
         if atai%10 == 0{
             data.textColor = UIColor.redColor()
             }else{
             data.textColor = UIColor.blackColor()
-
         }
-            }
+                       }
+        }
+    func sadParam(){
+        //sadBtnStatus
+        if atai>=0{
+            sadBtn.enabled = true
+        }else{
+            sadBtn.enabled = false
+        }
+
+    }
     
    
 
